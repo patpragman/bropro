62c3b84d2a49bdaa898fc092ecee573c6f5416de
Patrick Pragman <patrickpragman@pop-os.localdomain>
2023-01-12 07:24:18 -0900

    added better install script

--- setup.sh
+++ setup.sh
@@ -8,3 +8,13 @@ pip install -r requirements.txt
 
 echo "installing requirements"
 sudo apt install ffmpeg -y
+
+echo "initiating job for running the server on reboot"
+START_STRING="/home/pi/bropro/start.sh >> /home/pi/bropro/bropro.log &"
+INITIATOR_FILE="~/.profile"
+
+if grep -q "$START_STRING" "$INITIATOR_FILE"; then
+  echo $START_STRING >> $INITIATOR_FILE;
+else
+  echo "already installed...";
+fi
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source venv/bin/activate
-venv/bin/python -m flask run
+flask run --host=0.0.0.0
